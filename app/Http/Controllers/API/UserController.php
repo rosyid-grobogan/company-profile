d93a6dd01a49b6bcaf8170aecb9ae727ceca4981
mas rosyid <rosyidgrobogan@gmail.com>
2020-06-20 09:24:21 +0700

    until 27. install passport

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,10 @@ use App\User;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
     /**
      * Display a listing of the resource.
      *
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('home');
+Route::get('/users', 'HomeController@index')->name('home');
 Route::get('{path}', 'HomeController@index')->where('path', '([A-z\d-\/_.]+)?');
