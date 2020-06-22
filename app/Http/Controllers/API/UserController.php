ef7ef2fe97bf87d540383e68cab6bb7658027757
mas rosyid <rosyidgrobogan@gmail.com>
2020-06-22 09:38:03 +0700

    add save profile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -120,16 +120,27 @@ class UserController extends Controller
     public function updateProfile(Request $request)
     {
         $user = auth('api')->user();
-        //return ['message' => 'Success'];
-        //return $request->photo;
 
-        // if photo is exist
-        if($request->photo){
+        $this->validate($request, [
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users, email'.$user->id,
+            'password' => 'sometimes|required|min:6'
+        ]);
+
+
+        $currentPhoto = $user->photo;
+        if($request->photo != $currentPhoto){
             $name = time().'.'.explode('/', explode(':', substr( $request->photo, 0, strpos($request->photo,  ';') ))[1])[1];
 
-            \Image::make($request->photo)->save(public_path('img/profile').$name);
+            \Image::make($request->photo)->save( public_path('img/profile/').$name );
+
+            $request->merge([
+                'photo' => $name
+            ]);
         }
 
+        $user->update($request->all());
+        return ['message' => 'Success'];
         // data:image/png;base64,iVBORw0KGgo ..
     }
 }
